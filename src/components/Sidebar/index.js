f1033819b02e269d8aa78dff19fe877537bbd65e
lameed <lameedkunle@yahoo.com>
2024-09-20 20:45:48 +0100

    Update index.js
    add a fix

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/fav-big.png'
+import LogoSubtitle from '../../assets/images/lameed.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import {
